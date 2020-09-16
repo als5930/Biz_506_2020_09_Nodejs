1731df414ac0a321b4d52845c5311d1cb5a65696
als5930 <als3478@naver.com>
2020-09-16 16:27:09 +0900

    nodejs

--- Hello_Node_3/routes/index.js
+++ Hello_Node_3/routes/index.js
@@ -8,4 +8,16 @@ router.get("/", function (req, res, next) {
   res.render("index", { title: "반갑습니다", date: date, time: time });
 });
 
+router.post("/", function (req, res) {
+  let name = req.body.name;
+  let date = moment(new Date()).format("YYYY-MM-DD");
+  let time = moment(new Date()).format("HH:mm:ss");
+
+  res.render("index", {
+    title: name + " 님반갑습니다",
+    date: date,
+    time: time,
+  });
+});
+
 module.exports = router;
